144cc66807673ac5cfb6f37d7251155edf8c2bf0
qytek-dc <david@qytek.com>
2025-11-17 19:09:35 +0000

    Update version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, cmake_layout
 
 class ImsApiConan(ConanFile):
     name = "libims"
-    version = "2.0.0"
+    version = "2.0.2"
     license = "MIT"  # Adjust to match your actual license
     author = "Isomet Engineer <isomet@isomet.com>"
     url = "https://your.repo.url"  # Optional
